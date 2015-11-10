1ec156ecc2f1011d5aa01bd17788ad51d041ea19
saulocamposi <saulocamposi@gmail.com>
2015-11-10 14:41:21 -0200

    Started Prototype

---
+++ www/app/login/login.ts
@@ -0,0 +1,12 @@
+import {Page, NavController, ViewController} from 'ionic/ionic';
+
+
+@Page({
+  templateUrl:'app/login/login.html',
+})
+export class Login {
+  constructor(nav: NavController, view: ViewController) {
+    this.nav = nav;
+    this.view = view;
+  }
+}
---
+++ www/app/tabs/tabs.ts
@@ -0,0 +1,20 @@
+import {Page} from 'ionic/ionic';
+import {Dash} from '../dash/dash';
+import {Chats} from '../chats/chats';
+import {Account} from '../account/account';
+import {Login} from '../login/login';
+import {ProfileHairDresser} from '../profilehairdresser/profilehd';
+
+@Page({
+  templateUrl: 'app/tabs/tabs.html',
+})
+
+export class TabsPage {
+  constructor() {
+    this.DashRoot = Dash;
+    this.ChatsRoot = Chats;
+    this.AccountRoot = Account;
+    this.LoginRoot = Login;
+    this.ProfileHdRoot = ProfileHairDresser;
+  }
+}
