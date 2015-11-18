5743c0c62ee5213de40c4b3e7206b6eda04ed762
saulocamposi <saulocamposi@gmail.com>
2015-11-17 23:41:58 -0200

    insert of dynamic list

--- www/app/timelinehairdresser/timelinehairdresser.ts
+++ www/app/timelinehairdresser/timelinehairdresser.ts
@@ -3,27 +3,31 @@ import {Look} from '../look/look';
 import {LookSimple} from '../looksimple/looksimple';
 import {ProfileHairDresser} from '../profilehairdresser/profilehairdresser';
 import {TakePicture} from '../takepicture/takepicture';
-
+import {Clients} from '../data/client';
 
 @Page({
   templateUrl:'app/timelinehairdresser/timelinehairdresser.html',
+   providers: [Clients]
 })
 export class TimelineHairDresser {
-  constructor(nav: NavController, view: ViewController) {
+
+  constructor( clients: Clients, nav: NavController, view: ViewController) {
     this.nav = nav;
     this.view = view;
+    this.clients = clients;
+    this.looks = this.clients.all();
   }
-
+  
   showLook(){
-      this.nav.push(Look);
+    this.nav.push(Look);
   }
 
   showLookSimple(){
-      this.nav.push(LookSimple);
+    this.nav.push(LookSimple);
   }
 
   showProfile(){
-      this.nav.push(ProfileHairDresser);
+    this.nav.push(ProfileHairDresser);
   }
 
   takePicture(){
